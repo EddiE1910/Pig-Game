266ecbaafe85c1eb7cf7800fb09385bc05a34515
EddiE1910 <eddie191086@gmail.com>
2023-10-18 21:20:41 +0200

    Responiveness

--- js/game.js
+++ js/game.js
@@ -48,7 +48,7 @@ startGame.addEventListener("click", function () {
 // Game functions
 
 function setUpTurn() {
-    game.innerHTML = `<p><strong><u>Roll the dice for the ${gameData.players[gameData.index]}</u></strong></p>`;
+    game.innerHTML = `<p><strong><u>Roll the dice for ${gameData.players[gameData.index]}</u></strong></p>`;
     actionArea.innerHTML = '<button id="roll">Roll the Dice</button>';
     document.getElementById('roll').addEventListener('click', function () {
         throwDice();
@@ -72,7 +72,7 @@ function throwDice() {
 
     // if 2x 1 is rolled (Snake eyes!)
     if (gameData.rollSum === 2) {
-        game.innerHTML += '<h3><strong>Oh snap! Snake eyes!</strong></h3>';
+        actionArea.innerHTML += '<h3><strong>Oh snap! Snake eyes!</strong></h3>';
         gameData.score[gameData.index] = 0;
 
         // iternary operator for determine which player is next
@@ -88,14 +88,14 @@ function throwDice() {
     // if either die is a 1..
     else if (gameData.roll1 === 1 || gameData.roll2 === 1) {
         gameData.index ? (gameData.index = 0) : (gameData.index = 1);
-        game.innerHTML += `<h3><strong>Sorry, one of your rolls was a one, switching to ${gameData.players[gameData.index]}</strong></h3>`;
+        actionArea.innerHTML += `<h3><strong>Sorry, one of your rolls was a one, switching to ${gameData.players[gameData.index]}</strong></h3>`;
         setTimeout(setUpTurn, 1500);
     }
 
     // if neither die is a 1...
     else {
         gameData.score[gameData.index] = gameData.score[gameData.index] + gameData.rollSum;
-        actionArea.innerHTML = '<button id="rollagain">Roll again</button> <span>or</span> <button id="pass">Pass</button>';
+        actionArea.innerHTML = '<button id="rollagain">Roll</button> <span>&nbsp or &nbsp</span> <button id="pass">Pass</button>';
 
         document.getElementById('rollagain').addEventListener('click', function () {
             throwDice();
@@ -128,4 +128,50 @@ function throwDice() {
         score.innerHTML = `<h2><strong>${gameData.players[0]} score: ${gameData.score[0]}</strong><br></h2>`;
         score.innerHTML += `<h2><strong>${gameData.players[1]} score: ${gameData.score[1]}</strong></h2>`;
     } 
+
+
+    window.addEventListener('load', function () {
+        const posts = document.querySelectorAll('section');
+        let postTops = [];
+        let pageTop;
+        let counter = 1;
+        let prevCounter = 1;
+        let doneResizing;
+        // console.log(posts[0].getBoundingClientRect().top + window.pageYOffset);
+    
+       
+    
+    
+        // resize functions
+        this.window.addEventListener('resize', function(){
+            this.clearTimeout(doneResizing);
+            doneResizing = this.setTimeout(function(){
+
+                console.log('resize done!');
+                // resetPagePosition();
+            }, 500);
+        });
+    
+    
+        // function resetPagePosition() {
+        //     postTops = [];
+    
+        //     posts.forEach(function(post){
+        //         postTops.push( Math.floor( post.getBoundingClientRect().top + window.pageYoffset ) );
+        //     });
+    
+        //     const pagePosition = window.pageYoffset + 250;
+        //     counter = 0;
+    
+        //     postTops.forEach(function(post){
+        //         if (pagePosition > post) {
+        //             counter++;
+        //         }
+        //     });
+    
+        //     navLinks.forEach(function(eachLink){
+        //         eachLink.removeAttribute('class');
+        //     });
+    
+        });
 }
